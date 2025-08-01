bba58244eca80403ff9e8a8cf264dd513f5581dd
SenPie <smbat@duck.com>
2025-08-02 00:39:42 +0200

    Add script for market item

---
+++ objects/item/item.gd
@@ -0,0 +1,27 @@
+class_name MarketItem extends Area2D
+
+@onready var sound: AudioStreamPlayer2D = $Sound
+
+var mouse_over: bool = false
+var mouse_pos: Vector2 = Vector2.ZERO
+var picked: bool = false
+
+func _ready() -> void:
+	mouse_entered.connect( func() -> void: mouse_over = true )
+	mouse_exited.connect( func() -> void: mouse_over = false )
+
+func _physics_process(delta: float) -> void:
+	if picked:
+		global_position = lerp( global_position, mouse_pos, 0.2 )
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed() and mouse_over:
+			picked = true
+			mouse_pos = event.position
+			sound.play()
+		else:
+			picked = false
+	
+	if event is InputEventMouseMotion and picked:
+		mouse_pos = event.position
