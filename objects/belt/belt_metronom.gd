151874b09f9193efa345ddac065e685ab84e211d
SenPie <smbat@duck.com>
2025-08-02 18:21:27 +0200

    Add key to pause metronome with SPACE

--- objects/belt/belt_metronom.gd
+++ objects/belt/belt_metronom.gd
@@ -2,15 +2,24 @@ extends AudioStreamPlayer2D
 
 var bpm := 120.0
 
-func _ready() -> void:
+var timer : Timer
+
+func _init() -> void:
 	volume_db = -10
-	
-	var timer = Timer.new()
+
+	timer = Timer.new()
 	timer.wait_time = 60.0 / bpm
 	timer.autostart = true
 	timer.one_shot = false
 	timer.timeout.connect( _handle_timeout )
+
+func _ready() -> void:
 	add_child( timer )
 
 func _handle_timeout() -> void:
 	play()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			timer.paused = !timer.paused
