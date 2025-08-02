bc450027ac2f0a550a40b6b217f818bd5141974d
SenPie <smbat@duck.com>
2025-08-02 18:16:59 +0200

    Add modulo to not overflow belt sprite region x

--- objects/belt/belt_mover.gd
+++ objects/belt/belt_mover.gd
@@ -7,3 +7,7 @@ extends Sprite2D
 func _process(delta: float) -> void:
 	if working:
 		region_rect.position.x += moving_speed * delta
+
+		var region_pos_x := region_rect.position.x
+		var region_pos_x_sign : float = sign( region_pos_x )
+		region_rect.position.x = fmod( region_pos_x, 1000.0 )
