db6a1daaa5d367bfa611b67730cffee3d5b523dc
SenPie <smbat@duck.com>
2025-08-03 17:11:24 +0200

    Fix buttons

--- main_menu.gd
+++ main_menu.gd
@@ -5,7 +5,6 @@ func _on_start_pressed() -> void:
 	print("start pressed")
 	get_tree().change_scene_to_file("res://main.tscn")
 
-
 func _on_exit_pressed() -> void:
 	print("start pressed")
 	get_tree().quit()
